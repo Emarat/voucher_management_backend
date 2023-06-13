3d374e524735b61004766c490abb1bcf0caf2ec8
Penta <emarathossain00@gmail.com>
2023-06-13 12:41:46 +0600

    requisition form get all data

--- controller/loginController.js
+++ controller/loginController.js
@@ -4,24 +4,25 @@ const { getKeycloakInstance } = require('../config/keycloak-config');
 const bodyParser = require('body-parser');
 
 const keycloak = getKeycloakInstance();
+console.log(keycloak);
 
 // Set up middleware
 router.use(keycloak.middleware());
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
-
 // Define login route
 router.post('/login', (req, res) => {
-    const { username, password } = req.body;
-    keycloak.grantManager.obtainDirectly(username, password)
-        .then((grant) => {
-            req.kauth.grant = grant;
-            res.send('You are logged in!');
-        })
-        .catch((error) => {
-            res.status(401).send('Invalid username or password');
-        });
+  const { username, password } = req.body;
+  keycloak.grantManager
+    .obtainDirectly(username, password)
+    .then((grant) => {
+      req.kauth.grant = grant;
+      res.send('You are logged in!');
+    })
+    .catch((error) => {
+      res.status(401).send('Invalid username or password');
+    });
 });
 
 module.exports = router;
