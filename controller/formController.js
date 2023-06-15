4630cd27a05bdf11c66440a67730f33d1305909b
Penta <emarathossain00@gmail.com>
2023-06-15 11:17:04 +0600

    requisition type name included

--- controller/formController.js
+++ controller/formController.js
@@ -249,48 +249,64 @@ router.get('/requisitionType', async (req, res) => {
 router.get('/getAllData', async (req, res) => {
   try {
     const getDataQuery = `
-      SELECT DISTINCT
-        master.requisition_master_id,
-        master.requisition_type_id,
-        master.project_id,
-        master.requisition_id,
-        master.req_name,
-        master.supplier_id,
-        master.total_amount,
-        master.created_at,
-        details.uom,
-        details.unit_price,
-        details.qty,
-        details.amount,
-        rs.status_id,
-        rs.assigned_to,
-        comments.comments,
-        files.file_url,
-        customers.name,
-        vendor.vendor_name,
-        status.status_name,
-        category.category_name,
-        products.name AS product_name
-      FROM
-        requisition_master_data AS master
-      LEFT JOIN
-        requisition_details_data AS details ON master.requisition_master_id = details.requisition_master_id
-      LEFT JOIN
-        requisition_status AS rs ON master.requisition_master_id = rs.requisition_master_id
-      LEFT JOIN
-        comments ON master.requisition_master_id = comments.requisition_master_id
-      LEFT JOIN
-        requisition_file_details AS files ON master.requisition_master_id = files.requisition_master_id
-      LEFT JOIN
-        customers ON master.customer_id = customers.customer_id
-      LEFT JOIN
-        vendor ON master.supplier_id = vendor.vendor_id
-      LEFT JOIN
-        status ON rs.status_id = status.status_id
-      LEFT JOIN
-        category ON details.item_category_id = category.category_id
-      LEFT JOIN
-        products ON details.item_name = products.id;
+    SELECT DISTINCT
+    master.requisition_master_id,
+    master.requisition_type_id,
+    master.project_id,
+    master.requisition_id,
+    master.req_name,
+    master.supplier_id,
+    master.total_amount,
+    master.created_at,
+    details.uom,
+    details.unit_price,
+    details.qty,
+    details.amount,
+    rs.status_id,
+    rs.assigned_to, 
+    comments.user_id,
+    comments.req_id,
+    comments.comments,
+    files.file_url,
+    customers.name,
+    vendor.vendor_name,
+    status.status_name,
+    category.category_name,
+    products.name AS product_name,
+    requisition_type.requisition_type_name
+  FROM
+    requisition_master_data AS master
+  LEFT JOIN
+    requisition_details_data AS details
+    ON master.requisition_master_id = details.requisition_master_id
+  LEFT JOIN
+    requisition_status AS rs 
+    ON master.requisition_master_id = rs.requisition_master_id
+  LEFT JOIN
+    comments
+    ON master.requisition_master_id = comments.requisition_master_id
+  LEFT JOIN
+    requisition_file_details AS files
+    ON master.requisition_master_id = files.requisition_master_id
+  LEFT JOIN
+    customers
+    ON master.customer_id = customers.customer_id
+  LEFT JOIN
+    vendor
+    ON master.supplier_id = vendor.vendor_id
+  LEFT JOIN
+    status
+    ON rs.status_id = status.status_id
+  LEFT JOIN
+    category
+    ON details.item_category_id = category.category_id
+  LEFT JOIN
+    products
+    ON details.item_name = products.id
+  LEFT JOIN 
+      requisition_type
+      ON master.requisition_type_id = requisition_type.requisition_type_id;
+  
     `;
 
     const result = await pool.query(getDataQuery);
@@ -311,6 +327,7 @@ router.get('/getAllData', async (req, res) => {
         const newData = {
           requisition_master_id: row.requisition_master_id,
           requisition_type_id: row.requisition_type_id,
+          requisition_type_name: row.requisition_type_name,
           project_id: row.project_id,
           requisition_id: row.requisition_id,
           req_name: row.req_name,
